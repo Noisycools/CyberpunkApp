52a31e79a44ec83296f70925b91f3865657d6e4d
Aditya Nur Huda <adityanh1604@gmail.com>
2021-11-22 12:29:57 +0700

    dummy 1.1

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,6 @@ void main() {
   ));
 }
 
-void onPressed() {}
-
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
 
@@ -25,27 +23,89 @@ class Home extends StatelessWidget {
           ),
           centerTitle: true,
           backgroundColor: Colors.black54),
-      body: Center(
-        // child: Text(
-        //   'Welcome, V! \n Time to burn night city',
-        //   textAlign: TextAlign.center,
-        //   style: TextStyle(
-        //     fontSize: 26.0,
-        //     letterSpacing: 2.0,
-        //     color: HexColor("#fdfe0d"),
-        //     fontFamily: 'Cyberpunk',
-        //   ),
-        // ),
 
-        child: Image.asset('assets/cyberpunk-preorder.jpg'),
-      ),
+      // (=====CONTAINER AND PADDING=====)
+
+      // body: Container(
+      //   // padding: EdgeInsets.fromLTRB(10.0, 20.0, 30.0, 40.0),
+      //   padding: EdgeInsets.symmetric(horizontal: 128.0, vertical: 20.0),
+      //   margin: EdgeInsets.fromLTRB(0, 30.0, 0, 0),
+      //   color: Colors.grey[700],
+      //   child: Text(
+      //     'Welcome!',
+      //     style: TextStyle(
+      //       fontFamily: 'Aero',
+      //       fontSize: 20.999,
+      //     ),
+      //   ),
+      // ),
+
+      // (=====BODY = ROW=====)
+
+      // body: Row(
+      //   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+
+      //   children: [
+      //     TextButton(
+      //       onPressed: () {},
+      //       child: Text('More Info'),
+      //     ),
+      //     Container(
+      //       color: Colors.red,
+      //       child: Text(
+      //         'Pre-Order',
+      //         style: TextStyle(color: Colors.white),
+      //       ),
+      //       padding: EdgeInsets.symmetric(horizontal: 30.0, vertical: 10.0),
+      //     )
+      //   ],
+      // ),
+
+      // (=====BODY = CENTER=====)
+
+      // body: Center(
+
+      // (=====SIMPLE TEXT=====)
+
+      // child: Text(
+      //   'Welcome, V! \n Time to burn night city',
+      //   textAlign: TextAlign.center,
+      //   style: TextStyle(
+      //     fontSize: 26.0,
+      //     letterSpacing: 2.0,
+      //     color: HexColor("#fdfe0d"),
+      //     fontFamily: 'Cyberpunk',
+      //   ),
+      // ),
+
+      // (=====SAMPLE IMAGE=====)
+
+      // child: Image.asset('assets/cyberpunk-preorder.jpg'),
+
+      // (=====BUTTON=====)
+
+      // child: ElevatedButton.icon(
+      //   onPressed: () {},
+      //   icon: Icon(
+      //     Icons.account_balance_wallet,
+      //   ),
+      //   label: Text(
+      //     'Pre-Order',
+      //   ),
+      //   style: ElevatedButton.styleFrom(
+      //     primary: Colors.red,
+      //   ),
+      // ),
+      // ),
+
       backgroundColor: Colors.grey[850],
       floatingActionButton: FloatingActionButton(
-        onPressed: onPressed,
+        onPressed: () {},
         child: Text(
           'Menu',
           style: TextStyle(
             color: HexColor("#fdfe0d"),
+            fontFamily: 'Aero',
           ),
         ),
         backgroundColor: Colors.black54,
